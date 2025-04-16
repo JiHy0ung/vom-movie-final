33b7f9686366bcefb291717fe20972a5ff851e74
JiHyoung You <fbwlgud51@gmail.com>
2025-04-16 16:23:01 +0900

    change isLoading text color

--- src/pages/Homepage/components/Banner/Banner.jsx
+++ src/pages/Homepage/components/Banner/Banner.jsx
@@ -9,7 +9,7 @@ const Banner = () => {
   console.log("ddd", data);
 
   if (isLoading) {
-    return <h1>Loading...</h1>;
+    return <h1 className='text-white'>Loading...</h1>;
   }
   if (isError) {
     return <Alert variant="danger">{error.message}</Alert>;
