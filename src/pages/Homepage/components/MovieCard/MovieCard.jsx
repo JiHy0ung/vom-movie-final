f5398497a7413d184be99747b6d1ea6ed2757d41
JiHyoung You <fbwlgud51@gmail.com>
2025-04-20 19:40:02 +0900

    add hover event

--- src/pages/Homepage/components/MovieCard/MovieCard.jsx
+++ src/pages/Homepage/components/MovieCard/MovieCard.jsx
@@ -3,7 +3,6 @@ import { Badge } from "react-bootstrap";
 import "./MovieCard.style.css";
 
 const MovieCard = ({ movie }) => {
-
   const getPopularityLevel = (popularity) => {
     if (popularity <= 600) return "인기 중";
     if (popularity <= 800) return "핫한 영화";
@@ -22,14 +21,22 @@ const MovieCard = ({ movie }) => {
     >
       <div className="overlay">
         <h1 className="moviecard-title">{movie.title}</h1>
-        {movie.genre_ids.map((id) => (
-          <Badge bg="danger" className="moviecard-genre">
-            {id}
-          </Badge>
-        ))}
+        <div className="moviecard-genre-area">
+          {movie.genre_ids.map((id) => (
+            <Badge bg="danger" className="moviecard-genre">
+              {id}
+            </Badge>
+          ))}
+        </div>
         <div>
-          <div>평점: {movie.vote_average}점</div>
-          <div>{getPopularityLevel(movie.popularity)}</div>
+          <div className="moviecard-vote">
+            <span>평점: </span>
+            <span className="moviecard-vote-score">{movie.vote_average.toFixed(1)}</span>
+            <span>점</span>
+          </div>
+          <div className="moviecard-popularity">
+            {getPopularityLevel(movie.popularity)}
+          </div>
           <div>
             {movie.adult ? (
               <img
